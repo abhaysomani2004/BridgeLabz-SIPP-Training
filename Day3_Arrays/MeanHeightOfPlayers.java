18f4c9b41c2ae5d5182b8df59f4cb189d856fc97
Abhay Somani <abhaysomani2004@gmail.com>
2025-06-17 12:16:11 +0530

    [Abhay Somani] Arrays

---
+++ Day3_Arrays/MeanHeightOfPlayers.java
@@ -0,0 +1,20 @@
+package Day3_Arrays;
+import java.util.*;
+public class MeanHeightOfPlayers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of players:");
+        int n = sc.nextInt();
+        double[] heights = new double[n]; 
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter height of player " + (i + 1) + ":");
+            heights[i] = sc.nextDouble();
+        }
+        double sum = 0;
+        for (double height : heights) {
+            sum += height;
+        }
+        double meanHeight = sum / n;
+        System.out.printf("The mean height of the players is: %.2f\n", meanHeight);
+    }
+}
---
+++ Day3_Arrays/VoteGivingAbilityOfStudents.java
@@ -0,0 +1,27 @@
+package Day3_Arrays;
+import java.util.*;
+public class VoteGivingAbilityOfStudents {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of students:");    
+        int n = sc.nextInt();
+        int[] ages = new int[n]; 
+        int count = 0; 
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter age of student " + (i + 1) + ":");
+            ages[i] = sc.nextInt(); 
+        }
+        for (int i = 0; i < n; i++) {
+            if (ages[i] >= 18) { 
+                count++; 
+            }
+        }
+        System.out.println("Number of students eligible to vote: " + count);
+        System.out.println("Ages of eligible students:");
+        for (int i = 0; i < n; i++) {
+            if (ages[i] >= 18) { 
+                System.out.println(ages[i]);
+            }
+        }
+    }
+}
