34a5d5cb85d922029a1f20362cd320652a67a43b
Abhay Somani <abhaysomani2004@gmail.com>
2025-06-17 12:48:36 +0530

    [Abhay Somani] Arrays

---
+++ Day3_Arrays/FindFactorsAndStoreThemInArray.java
@@ -0,0 +1,25 @@
+package Day3_Arrays;
+import java.util.*;
+
+public class FindFactorsAndStoreThemInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to find its factors:");
+        int number = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+        System.out.println("The factors of " + number + " are:");
+        System.out.println(Arrays.toString(factors));
+    }
+}
