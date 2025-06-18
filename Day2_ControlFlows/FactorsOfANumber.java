dd198dee51b5803149c2f8f7fb44dc886a6a8798
Abhay Somani <abhaysomani2004@gmail.com>
2025-06-18 10:31:17 +0530

    [Abhay Somani] Control Flows Codes

--- Day2_ControlFlows/FactorsOfANumber.java
+++ Day2_ControlFlows/FactorsOfANumber.java
@@ -7,8 +7,8 @@ public class FactorsOfANumber {
         int number = sc.nextInt();
         System.out.println("The factors of " + number + " are:");
         for (int i = 1; i <= number; i++) {
-            if (number % i == 0) { // Check if 'i' is a factor of 'number'
-                System.out.print(i + " "); // Print the factor
+            if (number % i == 0) { 
+                System.out.print(i + " "); 
             }
         }
     }
