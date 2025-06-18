7f5117721513ddea5b15e88b8297dc0c72cc6b27
Abhay Somani <abhaysomani2004@gmail.com>
2025-06-18 10:18:24 +0530

    [Abhay Somani] Arrays

--- Day3_Arrays/StoreDigitsOfANumberAndStoreThemInArrayAndPrintLargestAndSecondLargest.java
+++ Day3_Arrays/StoreDigitsOfANumberAndStoreThemInArrayAndPrintLargestAndSecondLargest.java
@@ -5,20 +5,13 @@ public class StoreDigitsOfANumberAndStoreThemInArrayAndPrintLargestAndSecondLarg
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int number = sc.nextInt();
-        
-        // Convert the number to a string to easily access each digit
         String numStr = String.valueOf(number);
         int[] digits = new int[numStr.length()];
-        
-        // Store each digit in the array
         for (int i = 0; i < numStr.length(); i++) {
             digits[i] = Character.getNumericValue(numStr.charAt(i));
         }
-        
-        // Find the largest and second largest digits
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
-        
         for (int digit : digits) {
             if (digit > largest) {
                 secondLargest = largest;
@@ -27,7 +20,6 @@ public class StoreDigitsOfANumberAndStoreThemInArrayAndPrintLargestAndSecondLarg
                 secondLargest = digit;
             }
         }
-        
         System.out.println("Digits in the number: " + Arrays.toString(digits));
         System.out.println("Largest digit: " + largest);
         System.out.println("Second largest digit: " + secondLargest);
