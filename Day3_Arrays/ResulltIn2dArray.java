35f2ea3207398363526f010068e55037c968715d
Abhay Somani <abhaysomani2004@gmail.com>
2025-07-02 10:43:57 +0530

    [Abhay Somani] Arrays

--- Day3_Arrays/ResulltIn2dArray.java
+++ Day3_Arrays/ResulltIn2dArray.java
@@ -15,6 +15,7 @@ public class ResulltIn2dArray {
         if (percentage >= 90) grade = "A";
         else if (percentage >= 80) grade = "B";
         else if (percentage >= 70) grade = "C";
+
         else if (percentage >= 60) grade = "D";
         else grade = "F";
         String[][] result = {
